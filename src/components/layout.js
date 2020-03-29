89e41b541ad76d96b8649d5830ee8cf8bc95e0ae
tomppa2017 <thomas.w.crawley@gmail.com>
2020-03-29 21:27:16 +0100

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -36,7 +36,7 @@ class Layout extends React.Component {
       header = (
         <h3
           style={{
-            fontFamily: `Montserrat, sans-serif`,
+            fontFamily: `Times New Roman`,
             marginTop: 0,
           }}
         >
